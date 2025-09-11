459a291b749cefcf4ac32607ec4b03fe140db92b
barryhpr <barryhan@carleton.edu>
2025-09-11 00:55:29 -0500

    update

--- src/components/home/Schedule.jsx
+++ src/components/home/Schedule.jsx
@@ -92,7 +92,11 @@ const Schedule = () => {
             </td>
             {/* <td>Graph neural networks: model I</td> --- IGNORE --- */}
             <td>Paper reading & analysis</td>
-            <td>Writing task 2, out</td>
+            <td>
+              <a href="https://canvas.illinois.edu/courses/63144/assignments/1421864" target="_blank" rel="noopener noreferrer">
+                Writing task 2
+              </a>
+               , out</td>
             <td></td>
           </tr>
           <tr>
